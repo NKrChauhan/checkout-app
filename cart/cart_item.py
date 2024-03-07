f450e02c2d793a41b4ee356f0171ce764b5a4aae
Nitish Kumar <nitish@pushowl.com>
2024-03-07 19:19:58 +0530

    chores(cart): add comments for price calculation logic for CartItem

--- cart/cart_item.py
+++ cart/cart_item.py
@@ -17,12 +17,15 @@ class CartItem:
         discounted_bulk_quantities = list(self.product.bulk_price.keys())
         quantities_of_item = self.quantity
         cart_item_price = 0.0
+        # if bulk price discounts are available for the product then we calculate as per the special price offer
         if discounted_bulk_quantities:
             # Taking priority to higher value of quantity for eg:  4 for 18 over 2 for 9
             discounted_bulk_quantities.sort(reverse=True)
+            # we will try to group it together for each special price
             for discounted_bulk_quantity in discounted_bulk_quantities:
                 number_of_bulk_quantity_lot = quantities_of_item // discounted_bulk_quantity
                 quantities_of_item -= (number_of_bulk_quantity_lot * discounted_bulk_quantity)
                 cart_item_price = self.product.bulk_price[discounted_bulk_quantity] * number_of_bulk_quantity_lot
+        # we will calculate the price of the remaining quantity left after including special price offer
         cart_item_price += self.product.individual_price * quantities_of_item
         return cart_item_price
